f2cea839b17c491d10b3442822e7d8f2d17ea5f5
TheyCallMeBatus <105020741+juywvm@users.noreply.github.com>
2022-10-16 12:42:28 +0300

    Update w.lua

--- w.lua
+++ w.lua
@@ -1,1 +1,1 @@
-print("testloadstring")
+local v0=string.char;local v1=string.byte;local v2=string.sub;local v3=bit32 or bit;local v4=v3.bxor or v3.bxor;local v5=table.concat;local v6=table.insert;local function v7(v8,v9)local v10={};for i=1, #v8 do v6(v10,v0(v4(v1(v2(v8,i,i + 1)),v1(v2(v9,1 + ((i-1)% #v9),1 + ((i-1)% #v9) + 1)))%256));end return v5(v10);end
