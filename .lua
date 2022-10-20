841e3763011c0b3daa5bd033d6255bb9b6c41893
TheyCallMeBatus <105020741+juywvm@users.noreply.github.com>
2022-10-20 19:43:18 +0300

    Update .lua

--- .lua
+++ .lua
@@ -1,7 +1,188 @@
+local StringLib = {}
 
-print("woah")
-print("woah")
-print("woah")
-print("woah")
-print("woah")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/juywvm/w/main/.lua", true))()
+local TweenService = game:GetService('TweenService')
+local RunService = game:GetService('RunService')
+
+
+-- Glitch text setup
+local sub = string.sub
+local StringToTable = function(str)
+	local res = {}
+	for i = 1, #str do
+		res[i] = sub(str, i, i)
+	end
+	
+	return res
+end
+
+local Possible, createHash do
+	Possible = {
+		'-','+','*','/','|','}','{','[',']','~','\\','"',
+		':',';','?','.','>','<','=','+','-','_',')',
+		'(','*','&','%','$','#','@','!'
+	}
+	createHash = function(amount)
+		amount -= 1 -- idfk
+		if amount <= 0 then return '' end
+		local s = ''
+		
+		--[===[if amount >= 60 then -- table.concat is quicker than normal concatenation for larger strings
+			
+			local res = {}
+			for i = 1, amount do
+				res[i] = Possible[math.random(1, #Possible)]
+			end
+			
+			return table.concat(res, '')
+		end]===]
+		
+		for i = 1, amount do
+			s ..= Possible[math.random(1, #Possible)]
+		end
+		
+		return s
+	end
+
+	math.randomseed(tick() ^ 6)
+end
+
+local StylesFunction = {} do
+	StylesFunction.EasingStyle = function(self)
+		local FullString = self.FullString
+
+		local Int = Instance.new('IntValue')
+		Int.Value = 0 -- Hehe :)
+
+		local tween = TweenService:Create(Int, self.TweenInfo, {Value = #FullString})
+		self.tween = tween
+		tween:Play()
+		tween.Completed:Connect(function()self.Completed = true end)
+
+		local SingleHash = self.Hashed or createHash(#FullString)
+
+		while not self.Completed do task.wait()
+			local Hash = self.SingleHash and SingleHash:sub(self.Reversed and 1 or Int.Value, self.Reversed and #FullString-Int.Value or #FullString) or createHash(#FullString-Int.Value)
+			self.TextDisplay.Text = self.Reversed and Hash .. FullString:sub(#FullString-Int.Value+1, #FullString) or FullString:sub(1, Int.Value) .. Hash
+		end
+		self.Completed = true
+		Int:Destroy() -- destroy
+	end
+
+	StylesFunction.Suffix = function(self)
+		local FullString = self.FullString
+		local Suffix = self.Suffix or ''
+
+		local Int = Instance.new('IntValue')
+		Int.Value = 0
+
+		local tween = TweenService:Create(Int, self.TweenInfo, {Value = #FullString})
+		self.tween = tween
+		tween:Play()
+		tween.Completed:Connect(function()self.Completed = true end)
+
+		while not self.Completed do task.wait()
+			local Suffix = Int.Value == #FullString and '' or Suffix
+			self.TextDisplay.Text = FullString:sub(1, Int.Value) .. Suffix
+		end
+		self.Completed = true
+		Int:Destroy()
+	end
+	
+	StylesFunction.IndexUpdate = function(self)
+		local FullString = self.FullString
+
+		local Int = Instance.new('IntValue')
+		Int.Value = 0
+
+		local tween = TweenService:Create(Int, self.TweenInfo, {Value = #FullString})
+		self.tween = tween
+		tween:Play()
+		tween.Completed:Connect(function()self.Completed = true end)
+
+		local LastIndex, LastHash = 1, nil
+		while not self.Completed do task.wait()
+			local newHash = createHash(1)
+			if self.SingleHash and LastHash and Int.Value == LastIndex then
+				newHash = LastHash
+			end;LastHash = newHash;
+			self.TextDisplay.Text = FullString:sub(1, math.abs(Int.Value-1)) .. newHash .. FullString:sub(Int.Value+1)
+			LastIndex = Int.Value
+		end;self.TextDisplay.Text = FullString;
+		
+		self.Completed = true
+		Int:Destroy()
+	end
+end
+
+local StringAction = {} do
+	StringAction.__index = StringAction
+	function StringAction.new(FullString, TextDisplayer, info)
+		if type(FullString) ~= 'string' then
+			warn('Miss used type for first argument; Use a string instead.')
+		end
+		local self = setmetatable({}, StringAction)
+
+		self.FullString = FullString
+		self.TextDisplay = TextDisplayer
+		if info.Yielding == false then
+			self.Yielding = false
+		else
+			self.Yielding = true
+		end
+		self.Reversed = info.Reversed or false
+		self.SingleHash = info.SingleHash or false
+
+		self.Suffix = info.Suffix
+
+		self.TweenInfo = info.TweenInfo
+		
+		return self
+	end
+
+	function StringAction:GlitchText(style)
+		if StylesFunction[style] then
+			if self.Yielding == false then
+				coroutine.wrap(StylesFunction[style])(self)
+				return
+			end
+			StylesFunction[style](self)
+			return
+		end
+
+		if self.Yielding == false then
+			coroutine.wrap(StylesFunction.EasingStyle)(self)
+			return
+		end
+
+		StylesFunction.EasingStyle(self)
+	end
+
+	function StringAction:IsCompleted()
+		return self.Completed or false
+	end
+
+	function StringAction:Hash()
+		self.Hashed = createHash(#self.FullString)
+		self.TextDisplay.Text = self.Hashed
+	end
+
+	function StringAction:Cancel()
+		self.Completed = true
+		self.tween:Cancel()
+	end
+
+	function StringAction:Pause()
+		self.tween:Pause()
+	end
+
+	function StringAction:Resume()
+		self.tween:Play()
+	end
+
+	function StringAction:GetState()
+		return self.tween.PlaybackState
+	end
+end
+
+
+return
